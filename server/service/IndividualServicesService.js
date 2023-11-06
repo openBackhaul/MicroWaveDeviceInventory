8e6f4995d90ddee54d7415c7b9ce675e1a613864
Massimo Nardecchia <massimo.nardecchia@siaemic.com>
2023-11-06 12:30:12 +0100

    to implement service: /core-model-1-4:network-control-domain=cache/control-construct={mount-name}/logical-termination-point={uuid} Fixes #546

--- server/service/IndividualServicesService.js
+++ server/service/IndividualServicesService.js
@@ -2707,26 +2707,46 @@ exports.getCachedWredProfileConfiguration = function (url, user, originator, xCo
  * returns inline_response_200_29
  **/
 exports.getCachedLogicalTerminationPoint = function (url, user, originator, xCorrelator, traceIndicator, customerJourney, mountName, uuid, fields) {
-  return new Promise(function (resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-      "core-model-1-4:logical-termination-point": [{
-        "ltp-augment-1-0:ltp-augment-pac": {},
-        "layer-protocol": ["", ""],
-        "embedded-clock": [{}, {}],
-        "uuid": "uuid"
-      }, {
-        "ltp-augment-1-0:ltp-augment-pac": {},
-        "layer-protocol": ["", ""],
-        "embedded-clock": [{}, {}],
-        "uuid": "uuid"
-      }]
-    };
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
+  return new Promise(async function (resolve, reject) {
+    const myFields = user;
+    url = decodeURIComponent(url);
+    const parts = url.split('?');
+    url = parts[0];
+    //const fields = parts[1];
+    let correctMountname = null;
+    const appNameAndUuidFromForwarding = await resolveApplicationNameAndHttpClientLtpUuidFromForwardingName(url);
+//    let mountname = decodeURIComponent(url).match(/control-construct=([^/]+)/)[1];
+    let mountname = decodeMountName(url, false);
+    if (typeof mountname === 'object') {
+      resolve(Error(mountname[0].code, mountname[0].message));
+      return;
     } else {
-      resolve();
+      correctMountname = mountname;
     }
+    let returnObject = {};
+    const finalUrl = appNameAndUuidFromForwarding[1].url;
+    const correctUrl = modifyUrlConcatenateMountNamePlusUuid(finalUrl, correctMountname);
+    let result = await ReadRecords(correctMountname);
+    if (result != undefined) {
+      let finalJson = cacheResponse.cacheResponseBuilder(correctUrl, result);
+      if (finalJson != undefined) {
+        let objectKey = Object.keys(finalJson)[0];
+        finalJson = finalJson[objectKey];
+        if (myFields != undefined) {
+          var objList = [];
+          var rootObj = { value: "root", children: [] }
+          var ret = fieldsManager.decodeFieldsSubstringExt(myFields, 0, rootObj)
+          objList.push(rootObj)
+          fieldsManager.getFilteredJsonExt(finalJson, objList[0].children);
+        }
+        returnObject[objectKey] = finalJson;
+      } else {
+        returnObject = notFoundError();
+      }
+    } else {
+      returnObject = notFoundError();
+    }
+    resolve(returnObject);
   });
 }
 

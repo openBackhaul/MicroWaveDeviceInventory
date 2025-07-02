66ae93ae15a565de0a9337aa59f8e4858343de9e
Lorenzo Latta <lorenzo.latta@siaemic.com>
2025-07-02 21:06:25 +0200

    Fix issue #901

--- server/service/individualServices/alarmUpdater.js
+++ server/service/individualServices/alarmUpdater.js
@@ -55,26 +55,46 @@ exports.updateAlarmByTypeAndResource = function (json, alarmTypeId, resource, al
         // No More used
         // let maxIdentifier = Math.max(...alarms.map(alarm => parseInt(alarm[CURRENT_ALARM_ID])));
         // logger.debug("Alarm Max Identifier: " + maxIdentifier);
-        logger.info("Alarm id: " + alarmTypeId + " - on resource: " + resourceToUpdate + " - Added");
-        let newAlarm = {
-            // "current-alarm-identifier": (maxIdentifier + 1).toString(),
-            "timestamp": updatedAttributes.timestamp,
-            "resource": updatedAttributes.resource,
-            "alarm-type-id": updatedAttributes[ALARM_TYPE_ID],
-            "alarm-type-qualifier": updatedAttributes["alarm-type-qualifier"],
-            "alarm-severity": "alarms-1-0:SEVERITY_TYPE_" + updatedAttributes["alarm-severity"].toUpperCase()
-        };
-        alarms.push(newAlarm);
-        let numberOfCurrentAlarms = alarms.length;
-        json[objectKey][ALARMS_PAC][CURRENT_ALARMS][N_OF_CURRENT_ALARMS] = numberOfCurrentAlarms;
-        json[objectKey][ALARMS_PAC][CURRENT_ALARMS]["time-of-latest-change"] = newAlarm["timestamp"];
-        logger.debug("New Alarm added in the list");
+        logger.info("Alarm id: " + alarmTypeId + " - on resource: " + resourceToUpdate + " - to be added");
+        if (alarmStatus.toUpperCase().includes("CLEAR")) {
+            logger.warn("This is a CLEAR alarm type. Will not add to the list");
+        } else {
+            logger.info("Add to the list - Alarm id: " + alarmTypeId);
+            let resourceBuild = rebuildResource(updatedAttributes.resource);
+
+            let newAlarm = {
+                // "current-alarm-identifier": (maxIdentifier + 1).toString(),
+                "alarm-type-id": updatedAttributes[ALARM_TYPE_ID],
+                "alarm-type-qualifier": updatedAttributes["alarm-type-qualifier"],
+                "resource": resourceBuild,
+                "timestamp": updatedAttributes.timestamp,
+                "alarm-severity": updatedAttributes["alarm-severity"]
+            };
+
+            alarms.push(newAlarm);
+            let numberOfCurrentAlarms = alarms.length;
+            json[objectKey][ALARMS_PAC][CURRENT_ALARMS][N_OF_CURRENT_ALARMS] = numberOfCurrentAlarms;
+            json[objectKey][ALARMS_PAC][CURRENT_ALARMS]["time-of-latest-change"] = newAlarm["timestamp"];
+            logger.debug("New Alarm added in the list");
+        }
     } else {
         logger.debug("Alarm found in the list");
     }
 }
 
-function modifyResource (resource) {
+function modifyResource(resource) {
     let outputString = resource.replace(/\[uuid='(.*?)'\]/g, '=$1').replace(/\[local-id='(.*?)'\]/g, '=$1');
     return outputString;
+}
+
+function rebuildResource(resource) {
+    let step1 = resource.replace(/core-model-1-4:network-control-domain=live\/control-construct=.+?\//g, 'core-model-1-4:control-construct/');
+    let regex = /logical-termination-point=([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/i; // regex for logical termination point
+    let step2 = step1.replace(regex, "logical-termination-point[uuid='" + '$1' + "']");
+    let regex2 = /layer-protocol=([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/i; // regex for layer-protocol
+    let step3 = step2.replace(regex2, "layer-protocol[local-id='" + '$1' + "']");
+    let regex3 = /equipment=([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/i // regex for equipment
+    let step4 = step3.replace(regex3, "equipment[uuid='" + '$1' + "']");
+
+    return step4;
 }

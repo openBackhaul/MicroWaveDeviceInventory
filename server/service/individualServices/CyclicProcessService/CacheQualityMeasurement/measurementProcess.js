3ca8270165a0a7644d1ebcb788455f0fb8764bac
Prathiba <prathiba.jeevan.external@telefonica.com>
2025-08-20 15:44:54 +0200

    Incorporated changes from the deviceMetadataList code

--- server/service/individualServices/CyclicProcessService/CacheQualityMeasurement/measurementProcess.js
+++ server/service/individualServices/CyclicProcessService/CacheQualityMeasurement/measurementProcess.js
@@ -1,55 +1,22 @@
 const {
   diff
 } = require('deep-diff');
-const {
-  getCandidateDevice
-} = require('./deviceSelector');
+
 const onfAttributes = require('onf-core-model-ap/applicationPattern/onfModel/constants/OnfAttributes');
 const {
   getCachedControlConstruct,
   getLiveControlConstruct
 } = require('./controlConstructService');
+
 const {
   writeCacheQualityListToElasticsearch,
   readCacheQualityListFromElasticsearch
 } = require('./elasticService');
 
+const {getAllDeviceMetaData} = require('../DeviceMetaDataProcess/deviceMetaDataCyclicProcess.js');
 const utility = require('../../utility');
-
 let iteration = 1;
 
-// Prathiba > Ishu : this needs to be removed after making the metadatalist as global variable
-const deviceMetadataList = [{
-    "mount-name": "513250010",
-    "connection-status": "connected",
-    "changed-to-disconnected-time": "2025-07-15T12:44:21.010Z",
-    "added-to-device-list-time": "2025-07-15T12:44:21.010Z",
-    "last-complete-control-construct-update-time-attempt": "2025-07-15T12:44:21.010Z",
-    "last-successful-complete-control-construct-update-time": "2025-07-15T12:44:21.010Z",
-    "last-control-construct-notification-update-time": "2025-07-15T12:44:21.010Z",
-    "number-of-partial-updates-since-last-complete-update": 3,
-    "schema-cache-directory": "schema cache directory",
-    "device-type": "MLTN",
-    "vendor": "Ericsson",
-    "locked-status": false,
-    "exclude-from-qm": false
-  },
-  {
-    "mount-name": "513250006",
-    "connection-status": "connected",
-    "changed-to-disconnected-time": "2025-07-15T12:44:21.010Z",
-    "added-to-device-list-time": "2025-07-15T12:44:21.010Z",
-    "last-complete-control-construct-update-time-attempt": "2025-07-15T12:44:21.010Z",
-    "last-successful-complete-control-construct-update-time": "2025-07-15T12:44:21.010Z",
-    "last-control-construct-notification-update-time": "2025-07-15T12:44:21.010Z",
-    "number-of-partial-updates-since-last-complete-update": 3,
-    "schema-cache-directory": "schema cache directory",
-    "device-type": "ALCP2E",
-    "vendor": "SIAE",
-    "locked-status": false,
-    "exclude-from-qm": false
-  }
-];
 
 function calculateScore(differences) {
   let attributeMismatch = 0;
@@ -81,43 +48,17 @@ async function performQualityMeasurementAsPerCycle() {
   setTimeout(performQualityMeasurement, valueInSeconds * 1000);
 }
 
+
 async function performQualityMeasurement() {
-  try {
-    const result = {
-          'mount-name': "513250007",
-          'device-type': "ALCP2E",
-          'vendor': "SIAE",
-          'timestamp': new Date().toISOString(),
-          'attribute-mismatches': 5,
-          'attribute-class-mismatches': 4,
-          'weighted-score': 3
-        };
-
-        console.log(result);
-        let cacheQualityListFromElasticSearch = await readCacheQualityListFromElasticsearch()
-          .catch(error => {
-            throw error;
-          });
-
-        cacheQualityListFromElasticSearch.push(result);
-        let stringifiedResult = JSON.stringify(cacheQualityListFromElasticSearch);
-        await writeCacheQualityListToElasticsearch(stringifiedResult);
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-/**async function performQualityMeasurement() {
-  try {
-    
+  try {    
   console.log('*******************************************************************************************************');
   console.log('*        CACHE QUALITY MEASUREMENT PROCESS CYCLE - ITERATION ' + iteration++ + ' STARTED                                *');
   console.log('*                                                                                                     *');
   console.log('*                                 ( ' + utility.getTime() + ' )                                      *');
   console.log('*                                                                                                     *');
   console.log('*******************************************************************************************************');
-
-  // Prathiba > Ishu : "deviceMetadataList" needs to be replaced after making the metadatalist as global variable
+  
+  let deviceMetadataList = getAllDeviceMetaData(); 
   let deviceMetadataListForProcessing = [...deviceMetadataList];
   for (let index = 0; index < deviceMetadataListForProcessing.length; index++) {
     const device = deviceMetadataListForProcessing[index];
@@ -163,7 +104,6 @@ async function performQualityMeasurement() {
     console.log(error)
   }
 }
-  **/
 
 
 module.exports = {

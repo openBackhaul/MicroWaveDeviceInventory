6262e9e189f68aaf01e4e2545041e679cad01f5d
Lorenzo Latta <lorenzo.latta@techmahindra.com>
2026-09-08 12:16:52 +0200

    managing configurable server port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ var http = require('http');
 require('console-stamp')(console);
 
 var oas3Tools = require('openbackhaul-oas3-tools');
-var serverPort = 4024;
+
+// var serverPort = 4024;
+var serverPort;
+if (process.env.SERVER_PORT && process.env.SERVER_PORT != "") {
+  serverPort = parseInt(process.env.SERVER_PORT);
+} else {
+  serverPort = 4024
+}
+
 var appCommons = require('onf-core-model-ap/applicationPattern/commons/AppCommons');
 var individual = require('./service/IndividualServicesService');
 //var apiKeyAuth = require('./utils/apiKeyAuth');
